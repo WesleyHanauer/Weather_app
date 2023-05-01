10ff2581196a8f71fde1eecc115606300834f567
WesleyHanauer <whanauerbiz@proton.me>
2023-05-01 16:12:56 -0300

    Weather condition implemented

--- src/content/main.js
+++ src/content/main.js
@@ -4,19 +4,21 @@ import { getCityNameFromCoordinates } from '../backend/getCityNameFromCoordinate
 
 const openWeatherMapApiKey = "290858730fa1a80a3fbfec1f0eb4435d";
 
-async function getTemperature(latitude, longitude) {
+async function getTemperature(latitude, longitude, setWeatherCondition) {
   const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${openWeatherMapApiKey}&units=metric`);
   const data = await response.json();
+  const weatherCondition = data.weather[0].description;
+  setWeatherCondition(weatherCondition);
   return data.main.temp;
 }
 
-async function displayCoordinates(setCity, setCountry, setTemperature) {
+async function displayCoordinates(setCity, setCountry, setTemperature, setWeatherCondition) {
   try {
     const { latitude, longitude } = await getUserGeolocation();
     const { city, country } = await getCityNameFromCoordinates(latitude, longitude);
     setCity(city);
     setCountry(country);
-    const temperature = await getTemperature(latitude, longitude);
+    const temperature = await getTemperature(latitude, longitude, setWeatherCondition);
     setTemperature(temperature);
   } catch (error) {
     console.error(error);
@@ -27,9 +29,10 @@ function Main(){
     const [city, setCity] = useState('');
     const [country, setCountry] = useState('');
     const [temperature, setTemperature] = useState('');
+    const [weatherCondition, setWeatherCondition] = useState('');
 
     useEffect(() => {
-        displayCoordinates(setCity, setCountry, setTemperature);
+        displayCoordinates(setCity, setCountry, setTemperature, setWeatherCondition);
     }, []);
 
     return(
@@ -37,6 +40,7 @@ function Main(){
             <h1>Hello, world!</h1>
             <p>City: {city}, Country: {country}</p>
             <p>Temperature: {temperature}°C</p>
+            <p>Weather Condition: {weatherCondition}</p>
         </div>
     );
 }
